46c762904252a74120b03eb913cdd7b7d265c896
rhijmans <r.hijmans@gmail.com>
2021-11-10 19:15:46 -0800

    more generics

--- R/limeRate.R
+++ R/limeRate.R
@@ -3,14 +3,26 @@ if (!isGeneric("limeRate")) {setGeneric("limeRate", function(x, ...) standardGen
 
 setMethod("limeRate", signature(x="SpatRaster"), 
 	function(x, method, unit = "meq/100g", check_Ca=TRUE)  {
-		lapp(x, method=method, unit=unit, check_Ca = check_Ca)
+		# does not work, I think I know why and will look into it
+		#lapp(x, fun=lr, usenames=TRUE, method=method, unit=unit, check_Ca = check_Ca)
+
+		# for now:
+		limeRate(values(x), method=method, unit=unit, check_Ca=check_Ca)	
 	}
 )
 
-
 setMethod("limeRate", signature(x="data.frame"), 
 	function(x, method, unit = "meq/100g", check_Ca=TRUE)  {
-		lr(exch_ac=x$exch_ac, ECEC=x$ECEC, method=method, unit=unit, 	
-			SBD = x$SBD, SD = x$SD, check_Ca = check_Ca)
+		x <- as.list(x)
+		x$method=method 
+		x$unit=unit
+		x$check_Ca = check_Ca
+		do.call(lr, x)
+	}
+)
+
+setMethod("limeRate", signature(x="matrix"), 
+	function(x, method, unit = "meq/100g", check_Ca=TRUE)  {
+		limeRate(as.data.frame(x), method=method, unit=unit, check_Ca=check_Ca)
 	}
 )
