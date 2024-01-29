5e830604bf310f99db196c68dfc5d52348e4bcf3
rhijmans <r.hijmans@gmail.com>
2024-01-29 14:07:44 +0100

    fixed check

--- R/annualCost.R
+++ R/annualCost.R
@@ -6,7 +6,7 @@ if (!isGeneric("annualCost")) {setGeneric("annualCost", function(initial, mainte
 setMethod("annualCost", signature(initial="numeric", maintenance="numeric"), 
 	function(initial, maintenance, interest_rate, nyears) {
 
-		stopifnot((interest_rate >= 0) & (interest_rate < 100))
+		stopifnot(interest_rate >= 0)
 		interest_rate <- interest_rate / 100
 		stopifnot((initial >= 0) & (maintenance >= 0) & (nyears > 0))
 
@@ -31,7 +31,7 @@ setMethod("annualCost", signature(initial="numeric", maintenance="numeric"),
  
 setMethod("annualCost", signature(initial="SpatRaster", maintenance="ANY"), 
 	function(initial, maintenance, interest_rate, nyears) {
-		stopifnot((interest_rate >= 0) & (interest_rate < 1))
+		stopifnot(interest_rate >= 0)
 		interest_rate <- interest_rate / 100
 		stopifnot(nyears > 0)
 		stopifnot(inherits(maintenance, "SpatRaster") || inherits(maintenance, "numeric"))
