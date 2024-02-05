38fd95296e75597bb4a9ad17d346ef3efb7ddcf6
rhijmans <r.hijmans@gmail.com>
2024-02-05 12:35:20 +0100

    maint

--- R/profit.R
+++ R/profit.R
@@ -3,13 +3,14 @@ if (!isGeneric("profit")) {setGeneric("profit", function(cost, benefit, ...) sta
 
 
 setMethod("profit", signature(cost="numeric", benefit="numeric"), 
-	function(cost, benefit, nyears, interest_rate) {
+	function(cost, benefit, nyears, interest_rate, maintain=FALSE) {
 
 		stopifnot(interest_rate >= 0)
 		interest_rate <- interest_rate / 100
 		stopifnot((cost >= 0) & (benefit >= 0) & (nyears > 0))
 		# only compute for the first year, as the total amount will be the same
 		# for all years (while the balance between principal and interest changes)
+		nyears <- pmax(1, round(nyears))
 		if (interest_rate == 0) {
 			pay <- cost / nyears
 		} else {
@@ -17,19 +18,25 @@ setMethod("profit", signature(cost="numeric", benefit="numeric"),
 			principal_pay <- interest_rate * cost / ((1+interest_rate)^nyears - 1)
 			pay <- principal_pay + interest_pay
 		}
-		# average proportion of benefit 
-		avgben <- sapply(nyears, \(n) mean(seq(0, 1, 1/(n+1))[-1])
-		avgben * benefit - pay
+		if (maintain) {
+			benefit - (pay + pay / nyears)		
+		} else {
+			# average proportion of benefit 
+			avgben <- sapply(nyears, \(n) mean(seq(0, 1, 1/n)[-1]))
+			avgben * benefit - pay
+		}
 	}
 )
 
  
 setMethod("profit", signature(cost="SpatRaster", benefit="SpatRaster"), 
-	function(cost, benefit, nyears, interest_rate, filename="", overwrite=FALSE, ...) {
+	function(cost, benefit, nyears, interest_rate, maintain=FALSE, filename="", overwrite=FALSE, ...) {
 		stopifnot(inherits(nyears, "SpatRaster"))
 		stopifnot(inherits(interest_rate, "numeric"))
 		stopifnot(interest_rate >= 0)
 		interest_rate <- interest_rate / 100
+		
+		nyears <- max(round(nyears), 1)
 		if (interest_rate == 0) {
 			pay <- cost / nyears
 		} else {
@@ -37,9 +44,13 @@ setMethod("profit", signature(cost="SpatRaster", benefit="SpatRaster"),
 			principal_pay <- interest_rate * cost / ((1+interest_rate)^nyears - 1)
 			pay <- principal_pay + interest_pay
 		}
-		s <- sapply(1:50, \(n) mean(seq(0, 1, 1/(n+1))[-1]))
-		avgben <- subst(nyears, 1:50, s, 0.5)
-		out <- avgben * benefit - pay
+		if (maintain) {
+			out <- benefit - (pay + pay / nyears)		
+		} else {
+			s <- sapply(1:50, \(n) mean(seq(0, 1, 1/n)[-1]))
+			avgben <- subst(nyears, 1:50, s, 0.5)
+			out <- avgben * benefit - pay
+		}
 		if (filename != "") {
 			out <- writeRaster(out, filename, overwrite=overwrite, ...)
 		}
