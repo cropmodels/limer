f3ec6d1597fa326d3dd1a717d0e570c76d8d87aa
rhijmans <r.hijmans@gmail.com>
2024-11-12 10:58:36 -0800

    lapp

--- R/limeEffect.R
+++ R/limeEffect.R
@@ -1,44 +1,39 @@
 
-# Aramburu Merlos et al. 2023
-.lr_litas_convert <- function(exch_ac, ECEC, TAS, SBD, SD=20, a = 0.6, b = 0.2){
+.inverse_litas <- function(lime_rate, exch_ac, ECEC, SBD, SD=20, a = 0.6, b = 0.2) {
+	TAS <- seq(0, 100, 1)
 	tas <- TAS/100
 	lf <- 1/(a + tas * (b-a))
 	x <- lf * (exch_ac - tas * ECEC)
-	(x * SD * SBD)/20
-}
-
-
-.inverse_litas <- function(lime_rate, exch_ac, ECEC, SBD, ...) {
-	TAS <- seq(0, 100, 2)
-	LR <- .lr_litas_convert(exch_ac=exch_ac, ECEC=ECEC, TAS=TAS, SBD=SBD, ...)
+	LR <- (x * SD * SBD)/20
 	if (any(is.na(LR))) {
 		return(NA)
 	} else {
 		stats::approx(LR, TAS, xout=lime_rate, rule=2)$y 
 	}
-	#AS <- approx(LR, TAS, xout=lime_tha) 
-	#AS$y * ECEC/100 
 }
-
 
 
 if (!isGeneric("limeEffect")) {setGeneric("limeEffect", function(x, ...) standardGeneric("limeEffect"))}
 
 setMethod("limeEffect", signature(x="SpatRaster"), 
-	function(x, lime_rate, ..., filename="", overwrite=FALSE, wopt=list())  {
-		if (filename != "") {
-			if (!overwrite) {
-				if (file.exists(filename)) stop("file exists")
-			}
-		}
-		out <- terra::rast(x, nlyr=1)
-		terra::values(out) <- limeEffect(as.data.frame(x, na.rm=FALSE), lime_rate=lime_rate, ...)
-		if (filename != "") {
-			out <- terra::writeRaster(out, filename, overwrite=overwrite, wopt=wopt)
-		}
-		out
-		
-#		terra::lapp(x, fun = .inverse_litas, usenames=TRUE, lime_rate=lime_rate, ..., filename=filename, overwrite=overwrite, wopt=wopt )
+	function(x, lime_rate, ..., filename="", overwrite=FALSE, wopt=list()) {
+		.inv_lapp <- function(exch_ac, ECEC, SBD, lime_rate, SD=20, a=0.6, b=0.2) {
+			TAS <- seq(0, 100, 1)
+			tas <- TAS/100
+			lf <- 1/(a + tas * (b-a))
+
+			sapply(1:length(exch_ac), function(i) {
+					x <- lf * (exch_ac[i] - tas * ECEC[i])
+					LR <- (x * SD * SBD[i])/20
+					if (any(is.na(LR))) {
+						return(NA)
+					} else {
+						stats::approx(LR, TAS, xout=lime_rate, rule=2)$y 
+					}
+				} 
+			)
+		}		
+		terra::lapp(r, fun = .inv_lapp, usenames=TRUE, lime_rate=lime_rate[1], ..., filename="", overwrite=FALSE, wopt=list())
 	}
 )
 
@@ -46,9 +41,9 @@ setMethod("limeEffect", signature(x="SpatRaster"),
 setMethod("limeEffect", signature(x="data.frame"), 
 	function(x, lime_rate, ...) {
 		if (!is.null(x$SD)) {
-			sapply(1:nrow(x), function(i) .inverse_litas(lime_rate[1], x$exch_ac[i], x$ECEC[i], x$SBD[i], x$SD[i], ...))		
+			sapply(1:nrow(x), function(i) .inverse_litas(lime_rate, x$exch_ac[i], x$ECEC[i], x$SBD[i], x$SD[i], ...))		
 		} else {
-			sapply(1:nrow(x), function(i) .inverse_litas(lime_rate[1], x$exch_ac[i], x$ECEC[i], x$SBD[i], ...))
+			sapply(1:nrow(x), function(i) .inverse_litas(lime_rate, x$exch_ac[i], x$ECEC[i], x$SBD[i], ...))
 		}
 	}
 )
@@ -58,3 +53,4 @@ setMethod("limeEffect", signature(x="matrix"),
 		limeEffect(as.data.frame(x), lime_rate=lime_rate, ...)
 	}
 )
+
